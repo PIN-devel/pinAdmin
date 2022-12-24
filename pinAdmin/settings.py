77ed6c0e6bc54404a5b4d1cc3cd03c395d8e56cc
Innam Park <pin954562@gmail.com>
2022-12-24 15:32:22 +0900

    PIN-6 separate secret key

--- pinAdmin/settings.py
+++ pinAdmin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&y@&1k$9gkdq5$+0f9q%g3^+mo#7$4t3mf%+3x*y0qls6f^v9i'
+SECRET_KEY = os.environ['django']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
